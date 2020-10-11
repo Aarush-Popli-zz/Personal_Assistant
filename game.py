9f3e8ecdd117815b1610b857e5e33bc48031b359
Roshan Kumar <53408220+roshan9419@users.noreply.github.com>
2020-10-11 19:10:41 +0530

    Update game.py

--- game.py
+++ game.py
@@ -125,10 +125,10 @@ class LuckyTrivia:
 			ans = int(input("Ans: "))
 			if ans==correct:
 				print("You're Right!")
-				playsound.playsound('extrafiles/wow.mp3')
+				playsound.playsound('extrafiles/audios/wow.mp3')
 				self.totalScore += 1
 			else:
-				playsound.playsound('extrafiles/suspense.mp3')	
+				playsound.playsound('extrafiles/audios/suspense.mp3')	
 				print("You're Wrong!")
 				print("Correct Answer: ", options[correct-1])
 		except Exception as e:
@@ -224,12 +224,12 @@ def isContain(text, lst):
 def play(gameName):
 	speak('')
 	if isContain(gameName, ['dice','die']):
-		playsound.playsound('extrafiles/dice.mp3')
+		playsound.playsound('extrafiles/audios/dice.mp3')
 		result = "You got " + str(randint(1,6))
 		return result
 
 	elif isContain(gameName, ['coin']):
-		playsound.playsound('extrafiles/coin.mp3')
+		playsound.playsound('extrafiles/audios/coin.mp3')
 		p = randint(-10,10)
 		if p>0: return "You got Head"
 		else: return "You got Tail"
@@ -238,6 +238,7 @@ def play(gameName):
 		rockPaperScissorWindow()
 		return
 
+	'''
 	elif isContain(gameName, ['lucky','trivia','second']):
 		l = LuckyTrivia()
 		with open("extrafiles/lucky.txt", "r") as file:
@@ -246,10 +247,12 @@ def play(gameName):
 				l.nextQuestion(line[0],list(line[1:-1]), int(line[-1]))
 		result = l.showResult()
 		return result
+	'''
+	
 	else:
 		print("Game Not Available")
 
 
 def showGames():
-	return "1. Rock Paper Scissor\n2. Lucky Trivia\n3. Online Games"
+	return "1. Rock Paper Scissor\n2. Online Games"
 
