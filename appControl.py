178d8d5707749d54721eb53a87049d811df72ccf
Roshan Kumar <53408220+roshan9419@users.noreply.github.com>
2020-10-12 23:17:28 +0530

    Update appControl.py

--- appControl.py
+++ appControl.py
@@ -214,21 +214,25 @@ def systemInfo():
 	c = wmi.WMI()  
 	my_system_1 = c.Win32_LogicalDisk()[0]
 	my_system_2 = c.Win32_ComputerSystem()[0]
-	print("Total Disk Space:", round(int(my_system_1.Size)/(1024**3),2), "GB")
-	print("Free Disk Space:", round(int(my_system_1.Freespace)/(1024**3),2), "GB")
-	print("Manufacturer:", my_system_2.Manufacturer)
-	print("Model:", my_system_2. Model)
-	print("Owner:", my_system_2.PrimaryOwnerName)
-	print("Number of Processors:", my_system_2.NumberOfProcessors)
-	print("System Type:", my_system_2.SystemType)
+	info = ["Total Disk Space: " + str(round(int(my_system_1.Size)/(1024**3),2)) + " GB",
+			"Free Disk Space: " + str(round(int(my_system_1.Freespace)/(1024**3),2)) + " GB",
+			"Manufacturer: " + my_system_2.Manufacturer,
+			"Model: " + my_system_2. Model,
+			"Owner: " + my_system_2.PrimaryOwnerName,
+			"Number of Processors: " + str(my_system_2.NumberOfProcessors),
+			"System Type: " + my_system_2.SystemType]
+	return info
 
 def batteryInfo():
-	usage = str(psutil.cpu_percent(interval=0.1))
+	# usage = str(psutil.cpu_percent(interval=0.1))
 	battery = psutil.sensors_battery()
-	return usage, battery.percent
+	pr = str(battery.percent)
+	if battery.power_plugged:
+		return "Your System is currently on Charging Mode and it's " + pr + "% done."
+	return "Your System is currently on " + pr + "% battery life."
 
 def OSHandler(query):
 	if isContain(query, ['system', 'info']):
-		systemInfo()
+		return ['Here is your System Information...', '\n'.join(systemInfo())]
 	elif isContain(query, ['cpu', 'battery']):
 		return batteryInfo()
