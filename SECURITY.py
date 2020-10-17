4c3a3bbb101e87a742f74e35bb5ee97a6aeed2fa
Roshan Kumar <53408220+roshan9419@users.noreply.github.com>
2020-10-17 17:30:52 +0530

    Update SECURITY.py

--- SECURITY.py
+++ SECURITY.py
@@ -9,7 +9,14 @@ from threading import Thread
 from userHandler import UserData
 import FACE_UNLOCKER as FU
 
-background, textColor = 'black', 'white'
+background, textColor = 'black', '#fffff9'
+background, textColor = textColor, background
+
+avatarChoosen = 0
+choosedAvtrImage = None
+user_name = ''
+user_gender = ''
+
 try:
 	face_classifier = cv2.CascadeClassifier('Cascade/haarcascade_frontalface_default.xml')
 except Exception as e:
@@ -104,6 +111,8 @@ def startCapturing():
 		cap.release()
 		cv2.destroyAllWindows()
 		Thread(target=trainFace).start()
+		addBtn['text'] = '        Next        '
+		addBtn['command'] = lambda:raise_frame(root3)
 		return
 	
 	frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
@@ -116,24 +125,43 @@ def startCapturing():
 
 def Add_Face():
 
-	global cap
-	user = nameField.get()
-	gender = r.get()
-	if user != '' and gender!=0:
+	global cap, user_name, user_gender
+	user_name = nameField.get()
+	user_gender = r.get()
+	if user_name != '' and user_gender!=0:
 		if agr.get()==1:
 			cap = cv2.VideoCapture(0)
 			startCapturing()
 			progress_bar.place(x=20, y=273)
 			statusLbl['text'] = ''
-			gen = 'Male'
-			if gender==2: gen = 'Female'
-			u = UserData()
-			u.updateData(user, gen)
 		else:
 			statusLbl['text'] = '(Check the Condition)'
 	else:
 		statusLbl['text'] = '(Please fill the details)'
 
+
+def SuccessfullyRegistered():
+	if avatarChoosen != 0:
+		gen = 'Male'
+		if user_gender==2: gen = 'Female'
+		u = UserData()
+		u.updateData(user_name, gen, avatarChoosen)
+		usernameLbl['text'] = user_name
+		raise_frame(root4)
+
+def selectAVATAR(avt=0):
+	global avatarChoosen, choosedAvtrImage
+	avatarChoosen = avt
+	i=1
+	for avtr in (avtb1,avtb2,avtb3,avtb4,avtb5,avtb6,avtb7,avtb8):
+		if i==avt:
+			avtr['state'] = 'disabled'
+			userPIC['image'] = avtr['image']
+		else: avtr['state'] = 'normal'
+		i+=1
+
+
+################################################# GUI ###############################
 
 
 def raise_frame(frame):
@@ -151,8 +179,10 @@ if __name__ == '__main__':
 	# root.attributes('-toolwindow', True)
 	root1 = Frame(root, bg=background)
 	root2 = Frame(root, bg=background)
+	root3 = Frame(root, bg=background)
+	root4 = Frame(root, bg=background)
 
-	for f in (root1, root2):
+	for f in (root1, root2, root3, root4):
 		f.grid(row=0, column=0, sticky='news')	
 	
 	################################
@@ -214,7 +244,7 @@ if __name__ == '__main__':
 	#name
 	nameLbl = Label(userFrame2, text='Name', font=('Arial Bold', 12), fg=textColor, bg=background)
 	nameLbl.place(x=10,y=10)
-	nameField = Entry(userFrame2, bd=5, font=('Arial Bold', 10), width=25, relief=FLAT)
+	nameField = Entry(userFrame2, bd=5, font=('Arial Bold', 10), width=25, relief=FLAT, bg='#D4D5D7')
 	nameField.focus()
 	nameField.place(x=80,y=10)
 
@@ -243,11 +273,83 @@ if __name__ == '__main__':
 	statusLbl = Label(userFrame2, text='', font=('Arial 10'), fg=textColor, bg=background)
 	statusLbl.place(x=80, y=190)
 
-	###########################
-	####### CHAT SCREEN #######
-	###########################
+	##########################
+	#### AVATAR SELECTION ####
+	##########################
 	
+	Label(root3, text="Choose Your Avatar", font=('montserrat', 15), bg=background, fg=textColor).pack()
+
+	avatarContainer = Frame(root3, bg=background, width=300, height=500)
+	avatarContainer.pack(pady=10)
+	size = 100
+
+	avtr1 = Image.open('extrafiles/images/avatars/a1.png')
+	avtr1 = avtr1.resize((size, size))
+	avtr1 = ImageTk.PhotoImage(avtr1)
+	avtr2 = Image.open('extrafiles/images/avatars/a2.png')
+	avtr2 = avtr2.resize((size, size))
+	avtr2 = ImageTk.PhotoImage(avtr2)
+	avtr3 = Image.open('extrafiles/images/avatars/a3.png')
+	avtr3 = avtr3.resize((size, size))
+	avtr3 = ImageTk.PhotoImage(avtr3)
+	avtr4 = Image.open('extrafiles/images/avatars/a4.png')
+	avtr4 = avtr4.resize((size, size))
+	avtr4 = ImageTk.PhotoImage(avtr4)
+	avtr5 = Image.open('extrafiles/images/avatars/a5.png')
+	avtr5 = avtr5.resize((size, size))
+	avtr5 = ImageTk.PhotoImage(avtr5)
+	avtr6 = Image.open('extrafiles/images/avatars/a6.png')
+	avtr6 = avtr6.resize((size, size))
+	avtr6 = ImageTk.PhotoImage(avtr6)
+	avtr7 = Image.open('extrafiles/images/avatars/a7.png')
+	avtr7 = avtr7.resize((size, size))
+	avtr7 = ImageTk.PhotoImage(avtr7)
+	avtr8 = Image.open('extrafiles/images/avatars/a8.png')
+	avtr8 = avtr8.resize((size, size))
+	avtr8 = ImageTk.PhotoImage(avtr8)
+
+	
+	avtb1 = Button(avatarContainer, image=avtr1, bg=background, activebackground=background, relief=FLAT, bd=0, command=lambda:selectAVATAR(1))
+	avtb1.grid(row=0, column=0, ipadx=25, ipady=10)
+
+	avtb2 = Button(avatarContainer, image=avtr2, bg=background, activebackground=background, relief=FLAT, bd=0, command=lambda:selectAVATAR(2))
+	avtb2.grid(row=0, column=1, ipadx=25, ipady=10)
+
+	avtb3 = Button(avatarContainer, image=avtr3, bg=background, activebackground=background, relief=FLAT, bd=0, command=lambda:selectAVATAR(3))
+	avtb3.grid(row=1, column=0, ipadx=25, ipady=10)
+
+	avtb4 = Button(avatarContainer, image=avtr4, bg=background, activebackground=background, relief=FLAT, bd=0, command=lambda:selectAVATAR(4))
+	avtb4.grid(row=1, column=1, ipadx=25, ipady=10)
+
+	avtb5 = Button(avatarContainer, image=avtr5, bg=background, activebackground=background, relief=FLAT, bd=0, command=lambda:selectAVATAR(5))
+	avtb5.grid(row=2, column=0, ipadx=25, ipady=10)
+
+	avtb6 = Button(avatarContainer, image=avtr6, bg=background, activebackground=background, relief=FLAT, bd=0, command=lambda:selectAVATAR(6))
+	avtb6.grid(row=2, column=1, ipadx=25, ipady=10)
+
+	avtb7 = Button(avatarContainer, image=avtr7, bg=background, activebackground=background, relief=FLAT, bd=0, command=lambda:selectAVATAR(7))
+	avtb7.grid(row=3, column=0, ipadx=25, ipady=10)
+
+	avtb8 = Button(avatarContainer, image=avtr8, bg=background, activebackground=background, relief=FLAT, bd=0, command=lambda:selectAVATAR(8))
+	avtb8.grid(row=3, column=1, ipadx=25, ipady=10)
+
+
+	Button(root3, text='         Submit         ', font=('Arial Bold', 15), bg='#01933B', fg='white', bd=0, relief=FLAT, command=SuccessfullyRegistered).pack()
+
+	#########################################
+	######## SUCCESSFULL REGISTRATION #######
+	#########################################
+
+	userPIC = Label(root4, bg=background, image=avtr1)
+	userPIC.pack(pady=(40, 10))
+	usernameLbl = Label(root4, text="Roshan Kumar", font=('Arial Bold',15), bg=background, fg='#85AD4F')
+	usernameLbl.pack(pady=(0, 70))
+
+	Label(root4, text="Your account has been successfully activated!", font=('Arial Bold',15), bg=background, fg='#303E54', wraplength=300).pack(pady=10)
+	Label(root4, text="Launch the APP again to get started the conversation with your Personal Assistant", font=('arial',13), bg=background, fg='#A3A5AB', wraplength=350).pack()
+
+	Button(root4, text='     OK     ', bg='#0475BB', fg='white', font=('Arial Bold', 18), bd=0, relief=FLAT, command=lambda:quit()).pack(pady=50)
+
 	root.iconbitmap('extrafiles/images/assistant2.ico')
 	raise_frame(root1)
 	root.mainloop()
-
